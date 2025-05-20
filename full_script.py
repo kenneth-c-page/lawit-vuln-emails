f16f5ad0c9270bf8e07f55dc2c4df6a41464f30c
Marc Page <MarcAllenPage@gmail.com>
2025-05-20 06:07:48 -0500

    Just adding devopsdrive settings (and black formatting)

--- full_script.py
+++ full_script.py
@@ -9,8 +9,10 @@ from requests_oauthlib import OAuth2Session
 from oauthlib.oauth2 import BackendApplicationClient
 from email.message import EmailMessage
 from email.mime.text import MIMEText
+from devopsdriver import Settings
 
-class Assignee():
+
+class Assignee:
     def __init__(self, cat=None, machines=None, name=None, email=None, loc=None):
         self.cat = cat
         self.machines = machines if machines is not None else []
@@ -23,44 +25,44 @@ class Assignee():
             self.cat = cat
         else:
             raise TypeError(f"{cat} is NOT a string.")
-        
+
     def set_machines(self, machines):
         if type(machines) == list:
             self.machines = machines
         else:
             self.machines = [machines]
-    
+
     def add_machines(self, machines):
         if type(machines) == list:
             self.machines += machines
         else:
             self.machines.append(machines)
-    
+
     def set_location(self, loc):
         self.loc = loc
-    
+
     def set_name(self, name):
         self.name = f"{name}"
-    
+
     def set_email(self, email):
         self.email = f"{email}"
-    
+
     def get_type(self):
         return self.cat
-    
+
     def get_name(self):
         return self.name
-    
+
     def get_machines(self):
         return self.machines
-    
+
     def get_email(self):
         return self.email
-    
+
     def get_location(self):
         return self.loc
-    
-    def format_email(self):
+
+    def format_email(self, settings: Settings):
         if self.machines:
             formatted_vulnerabilities = []
             for machine in self.machines:
@@ -72,27 +74,25 @@ class Assignee():
                                 sanitized += f"\n\t- Adobe Products"
                         else:
                             sanitized += f"\n\t- {vuln}"
-                    vulns = f"The device labeled {machine.tag} has the following vulnerabilities that need to be updated or removed:\n\t- A Windows/Mac system update if available{sanitized}"
+                    vulns = settings["vulnerability"].format(machine.tag, sanitized)
                     formatted_vulnerabilities.append(vulns)
-            content = """
-Hi {0}!
-
-In an effort to protect you and important assets, we are emailing you because your law school computer(s) has/have been found with the following vulnerabilities:
-
-{1}
-
-If you use these programs, please update them at your earliest convenience. However, if you do not use these programs, please remove them from your device.
-
-If you have any questions or need help with removing these vulnerabilities, please feel free to email, call, or visit us at the Help Desk.
-
-Thank you!
-Best regards,
-Kenneth Page
-        """.format(self.name.split()[0].title(), f"\n".join(formatted_vulnerabilities))
+            content = settings["email body"].format(
+                self.name.split()[0].title(), f"\n".join(formatted_vulnerabilities)
+            )
             return content
 
-class Machine():
-    def __init__(self, name=None, users=None, loc=None, tag=None, vulns=None, alias=None, falcon_count=None):
+
+class Machine:
+    def __init__(
+        self,
+        name=None,
+        users=None,
+        loc=None,
+        tag=None,
+        vulns=None,
+        alias=None,
+        falcon_count=None,
+    ):
         self.users = users if users is not None else []
         self.loc = loc
         self.tag = tag
@@ -107,7 +107,7 @@ class Machine():
             self.users = user_list
         else:
             self.users = [user_list]
-    
+
     def add_users(self, user_list):
         try:
             if type(user_list) == list:
@@ -122,7 +122,7 @@ class Machine():
             self.vulns = vuln_list
         else:
             self.vulns = [vuln_list]
-    
+
     def add_vulns(self, vuln_list):
         try:
             if type(vuln_list) == list:
@@ -131,68 +131,76 @@ class Machine():
                 self.vulns.append(vuln_list)
         except:
             self.vulns = vuln_list if type(vuln_list) == list else [vuln_list]
-    
+
     def set_tag(self, tag):
         self.tag = tag
-    
+
     def set_loc(self, loc):
         self.loc = loc
 
     def set_alias(self, alias):
         self.alias = alias
-    
+
     def set_falcon_count(self, count):
         self.falcon_count = count
 
     def get_users(self):
         return self.users
-    
+
     def get_location(self):
         return self.loc
-    
+
     def get_vulns(self):
         return self.vulns
-    
+
     def get_tag(self):
         return self.tag
-    
+
     def get_alias(self):
         return self.alias
-    
+
     def get_name(self):
         return self.name
 
     def assgin(self):
         self.assign = True
-    
+
     def unassign(self):
         self.assign = False
-    
+
     def get_assignment(self):
         return self.users if self.users else False
-    
+
     def get_falcon_count(self):
         return self.falcon_count
 
-def get_snipe(endpoint):
-    URL = f"https://jrcb-snipe-it.byu.edu/api/v1/{endpoint}"
-    api_token = ""
-    HEADERS = {"Authorization":"Bearer " + api_token,"Content-Type":"application/json","Accept":"application/json"}
+
+def get_snipe(endpoint, settings: Settings):
+    HEADERS = {
+        h: v.format(settings["snipe.token"])
+        for h, v in settings["snipe.headers"].items()
+    }
     all_rows = []
     offset = 0
+
     while True:
-        new_url = f"{URL}?limit=500&offset={offset}"
-        response = requests.get(url = new_url, headers = HEADERS).json()
+        new_url = settings["snipe.url"].format(
+            endpoint, settings["snipe.pagination"], offset
+        )
+        response = requests.get(
+            url=new_url, headers=HEADERS, timeout=settings["snipe.timeout in seconds"]
+        ).json()
         total = response.get("total", 0)
         rows = response.get("rows", [])
         all_rows.extend(rows)
-        if len(rows) < 500 or offset + len(rows) >= total:
+        if len(rows) < settings["snipe.pagination"] or offset + len(rows) >= total:
             break
-        offset += 500
+        offset += settings["snipe.pagination"]
     return all_rows
 
-def parse_snipe_assets():
-    assets = get_snipe("hardware")
+
+def parse_snipe_assets(settings: Settings):
+    assets = get_snipe("hardware", settings)
     mapping = []
     for asset in assets:
         assignee = asset["assigned_to"]
@@ -200,7 +208,7 @@ def parse_snipe_assets():
         if not asset["category"]["name"] in ["Printer", "Monitors"]:
             new_mach = Machine(name=device, users=assignee, tag=asset["asset_tag"])
 
-            if (asset["location"] or asset["rtd_location"]):
+            if asset["location"] or asset["rtd_location"]:
                 try:
                     new_mach.set_loc(asset["location"]["name"])
                 except:
@@ -210,8 +218,9 @@ def parse_snipe_assets():
             mapping.append(new_mach)
     return mapping
 
-def parse_snipe_users():
-    users = get_snipe("users")
+
+def parse_snipe_users(settings: Settings):
+    users = get_snipe("users", settings)
     mapping = []
     for user in users:
         try:
@@ -221,59 +230,65 @@ def parse_snipe_users():
             pass
     return mapping
 
-def get_okta():
-    URL = "https://ces-byulaw-admin.okta.com/api/v1/users"
-    api_token = ""
-    HEADERS = {"Authorization":"SSWS " + api_token,"Accept":"application/json"}
+
+def get_okta(settings: Settings):
+    URL = settings["okta.url"]
+    HEADERS = {
+        h: v.format(settings["okta.token"]) for h, v in settings["okta.headers"].items()
+    }
     users = []
     while URL:
-        r = requests.get(url = URL, headers = HEADERS)
+        r = requests.get(
+            url=URL, headers=HEADERS, timeout=settings["okta.timeout in seconds"]
+        )
         users += r.json()
 
         link_header = r.headers["link"]
-        if link_header:# and 'rel="next"' in link_header:
+        if link_header:  # and 'rel="next"' in link_header:
             # match = re.findall(r'<([^>]+)>; rel="next"', link_header)
-            match = r.links.get('next', {}).get('url')
+            match = r.links.get("next", {}).get("url")
             # URL = match[0] if match else None
             URL = match if match else None
         else:
             URL = None
     return users
 
-def parse_okta():
-    users = get_okta()
+
+def parse_okta(settings: Settings):
+    users = get_okta(settings)
     mapping = {}
     for user in users:
-        profile = user['profile']
-        if not "student" in "".join([pos for pos in profile['position']]):
-            mapping[f"{profile["firstName"]} {profile["lastName"]}"] = {"login":profile["login"], "email":profile["email"]}
+        profile = user["profile"]
+        if not "student" in "".join([pos for pos in profile["position"]]):
+            mapping[f"{profile["firstName"]} {profile["lastName"]}"] = {
+                "login": profile["login"],
+                "email": profile["email"],
+            }
         # mapping[f"{user["displayName"]}"] = {"login":profile["login"], "email":profile["email"]}
     return mapping
 
-def get_threatdown():
-    # Replace with your actual credentials from ThreatDown OneView
-    ACCOUNT_ID = ""
-    CLIENT_ID = ""
-    CLIENT_SECRET = ""
 
-    # Base URL for ThreatDown API
-    BASE_URL = "https://api.threatdown.com"
-    REPORT_ID = ""
+def get_threatdown(settings: Settings):
+    # Replace with your actual credentials from ThreatDown OneView
+    ACCOUNT_ID = settings["threatdown.account"]
+    CLIENT_ID = settings["threatdown.client"]
+    CLIENT_SECRET = settings["threatdown.secret"]
+    REPORT_ID = settings["threatdown.report"]
 
     def get_oauth_client(client_id, client_secret, account_id):
         """Authenticate and return an OAuth2 session client."""
-        client_scope = ["read", "write", "execute"]  # Adjust scopes based on API requirements
+        client_scope = settings["threatdown.scope"]
         headers = {"accountid": account_id}
-        
+
         client = BackendApplicationClient(client_id=client_id, scope=client_scope)
         session = OAuth2Session(client=client, scope=client_scope)
         session.headers.update(headers)
-        
+
         try:
             token = session.fetch_token(
-                token_url=f"{BASE_URL}/oauth2/token",
+                token_url=settings["threatdown.token url"],
                 client_secret=client_secret,
-                scope=" ".join(client_scope)
+                scope=" ".join(client_scope),
             )
             return session
         except Exception as e:
@@ -284,34 +299,33 @@ def get_threatdown():
         reports = []
         next_cursor = ""
         page_size = 1000  # Adjust based on API limits or documentation
-        url = f"{BASE_URL}/nebula/v1/reports/{REPORT_ID}"
+        url = settings["threatdown.reports url"].format(REPORT_ID)
         token = client.fetch_token(
-                token_url=f"{BASE_URL}/oauth2/token",
-                client_secret=CLIENT_SECRET,
-                scope=" ".join(["read", "write", "execute"]))
-        HEADERS = {"content-type":"application/json","authorization": f"Bearer {token["access_token"]}","accountid":ACCOUNT_ID}
-
-        exp_url = "https://api.threatdown.com/nebula/v1/cve/export"
-        exp_report = []
-        # while True:
-        EXP_BODY = {
-            "format":"json",
-            "download":True,
-            "select":[{"field":"host_name","newField":"Name"},
-                    {"field":"product","newField":"Application"},
-                    {"field":"machine_id","newField":"Machine_ID"},
-                    {"field":"alias","newField":"Alias"},
-                    {"field":"fully_qualified_host_name","newField":"Full_Hostname"}],
-            "groups":[{"installation_date_after":"2000-01-01T12:00:00Z"},{"page_size":2000}]
+            token_url=settings["threatdown.token url"],
+            client_secret=CLIENT_SECRET,
+            scope=" ".join(settings["threatdown.scope"]),
+        )
+        HEADERS = {
+            "content-type": "application/json",
+            "authorization": f"Bearer {token["access_token"]}",
+            "accountid": ACCOUNT_ID,
         }
 
-        exp_report += client.post(exp_url, headers = HEADERS, data=json.dumps(EXP_BODY)).json()
+        exp_url = settings["threatdown.reports url"]
+        exp_report = []
+        # while True:
+        EXP_BODY = settings["threatdown.export body"]
+
+        exp_report += client.post(
+            exp_url, headers=HEADERS, data=json.dumps(EXP_BODY)
+        ).json()
         return exp_report
-    
+
     return get_all_reports(get_oauth_client(CLIENT_ID, CLIENT_SECRET, ACCOUNT_ID))
 
-def parse_threatdown():
-    vulns = get_threatdown()
+
+def parse_threatdown(settings: Settings):
+    vulns = get_threatdown(settings)
     mapping = {}
     for vuln in vulns:
         name = vuln["Name"]
@@ -321,36 +335,45 @@ def parse_threatdown():
         except:
             alias = None
         try:
-            mapping[name.upper()]["apps"] += [app] if not app in mapping[name]["apps"] else []
+            mapping[name.upper()]["apps"] += (
+                [app] if not app in mapping[name]["apps"] else []
+            )
         except:
-            mapping[name.upper()] = {"apps":[app],"alias":alias.upper() if alias else None}
+            mapping[name.upper()] = {
+                "apps": [app],
+                "alias": alias.upper() if alias else None,
+            }
     return mapping
 
-def get_falcon():
-    falcon_token = ""
-    URL = "https://oit-humio.byu.edu/api/v1/repositories/law_oit_shared/query"
-    query = '"remediation_actions": "Microsoft" and "hostname": not "JRCB*" and "hostname": not "PROX" | groupBy(hostname, limit="max") | sort(_count)'
-    payload = {
-        "queryString":query,
-        "start":"31days",
-        "end":"now",
-        "isLive":False
-    }
+
+def get_falcon(settings: Settings):
+    URL = settings["falcon.url"]
+    payload = settings["falcon.payload"]
     HEADERS = {
-        "Authorization":f"Bearer {falcon_token}",
-        "Content-Type":"application/json",
-        "Accept":"application/json"
+        h: v.format(settings["falcon.token"])
+        for h, v in settings["falcon.headers"].items()
     }
-    response = requests.post(URL, headers=HEADERS, data=json.dumps(payload)).json()
+    response = requests.post(
+        URL,
+        headers=HEADERS,
+        data=json.dumps(payload),
+        timeout=settings["falcon.timeout in seconds"],
+    ).json()
     return response
 
-def parse_falcon():
-    assets = get_falcon()
-    mapping = [Machine(name=asset["hostname"],falcon_count=int(asset["_count"])) for asset in assets]
+
+def parse_falcon(settings: Settings):
+    assets = get_falcon(settings)
+    mapping = [
+        Machine(name=asset["hostname"], falcon_count=int(asset["_count"]))
+        for asset in assets
+    ]
     return mapping
+
 
 def falcon_email():
     pass
+
 
 def add_okta_emails(mapping, users):
     for user in users:
@@ -358,6 +381,7 @@ def add_okta_emails(mapping, users):
         match, score = find_match(name, mapping.keys())
         if score >= 0.8:
             user.set_email(mapping[match]["email"])
+
 
 def add_vulns(mapping, machines):
     unused_threatdown = [key for key in mapping.keys()]
@@ -390,13 +414,14 @@ def add_vulns(mapping, machines):
                                 vulns = mapping[a_match]["apps"]
                         else:
                             pass
-                                # unused_threatdown.remove(a_match)
-                                #### ADD TO CATCH-ALL USER
+                            # unused_threatdown.remove(a_match)
+                            #### ADD TO CATCH-ALL USER
                 except:
                     vulns = []
         except:
-            vulns=[]
+            vulns = []
         machine.set_vulns(vulns)
+
 
 def map_machines(users, machine_list):
     no_loc_u = []
@@ -421,8 +446,17 @@ def map_machines(users, machine_list):
                     machine.add_users(user.get_name())
         if not machine.get_assignment():
             no_loc_m.append(machine)
-    users.append(Assignee(name="459 JRCB", cat="loc", machines=[mach for mach in no_loc_m], loc="459 JRCB", email=""))
-            
+    users.append(
+        Assignee(
+            name="459 JRCB",
+            cat="loc",
+            machines=[mach for mach in no_loc_m],
+            loc="459 JRCB",
+            email="",
+        )
+    )
+
+
 def send_emails(users):
     with open("vuln2.0", "w") as f:
         f.write("")
@@ -445,9 +479,11 @@ def send_emails(users):
         #     s.quit()
     pass
 
+
 def compare_names(n1, n2):
     score = SequenceMatcher(None, n1.upper(), n2.upper()).ratio()
     return score
+
 
 def find_match(name, names):
     best_match = ""
@@ -459,23 +495,26 @@ def find_match(name, names):
             best_match = n
     return best_match, best_score
 
+
 def main():
+    settings = Settings(__file__).key("secrets").cli("cli").env("env")
     month = int(datetime.datetime.now().month)
-    users = parse_snipe_users()
+    users = parse_snipe_users(settings)
     if month % 2 == 1:
-        assets = parse_snipe_assets()
-        users_to_emails = parse_okta()
+        assets = parse_snipe_assets(settings)
+        users_to_emails = parse_okta(settings)
         add_okta_emails(users_to_emails, users)
-        vulns = parse_threatdown()
+        vulns = parse_threatdown(settings)
         add_vulns(vulns, assets)
         ####### NEXT STEPS #######
         map_machines(users, assets)
         send_emails(users)
     else:
-        assets = parse_falcon()
-        vulns = parse_threatdown()
+        assets = parse_falcon(settings)
+        vulns = parse_threatdown(settings)
         add_vulns(vulns, assets)
-        falcon_email() # DEFINE
+        falcon_email()  # DEFINE
+
 
 if __name__ == "__main__":
     main()
